f2c775ecb0fc0df8b9254769f2df7583b4f0f523
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2022-08-11 18:24:34 -0400

    Reformat File Structure

--- src/main/cpp/examples/ExampleCommand.cpp
+++ src/main/cpp/examples/ExampleCommand.cpp
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include "commands/ExampleCommand.h"
+#include "examples/ExampleCommand.h"
 
 ExampleCommand::ExampleCommand(ExampleSubsystem *subsystem)
     : m_subsystem{subsystem} {}
