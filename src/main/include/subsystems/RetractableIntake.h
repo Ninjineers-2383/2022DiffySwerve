9ebf66480968fceea5fb73828595de4eac45f66f
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2022-08-29 01:06:25 -0400

    Lift Stuff

--- src/main/include/subsystems/RetractableIntake.h
+++ src/main/include/subsystems/RetractableIntake.h
@@ -16,9 +16,13 @@ public:
      * @param solDown pcm port num for down actuation
      */
     RetractableIntake(int motorPort, int solUp, int solDown);
+    RetractableIntake(int motorPort, int solUp, int solDown, bool inactiveWhenRetracted);
+    void RetractableIntake::toggleLift(frc::DoubleSolenoid::Value value);
+    frc::DoubleSolenoid::Value RetractableIntake::getState();
+    void RetractableIntake::intake(int power);
     frc::DoubleSolenoid m_lift;
     WPI_TalonFX m_motor;
-
+    bool RetractableIntake::inactiveWhenRetracted;
     /**
      * Will be called periodically whenever the CommandScheduler runs.
      */
