fc71e021c0637aa18443b126988e1f1ca43de0ea
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2022-09-16 07:57:47 -0400

    Comments

--- src/main/include/subsystems/CTREDoubleEncoder.h
+++ src/main/include/subsystems/CTREDoubleEncoder.h
@@ -11,14 +11,37 @@
 class CTREDoubleEncoder
 {
 public:
+    /**
+     * Differential Swerve Module
+     *
+     * @param portA DIO pin number of quadrature channel A
+     * @param portB DIO pin number of quadrature channel B
+     * @param portAbs port number of the DutyCycle channel of the absolute encoder
+     */
     CTREDoubleEncoder(const int portA, const int portB, const int portAbs);
 
+    // Reset quadrature encoder
     void ResetEncoders();
+
+    // Set quadrature offset with absolute encoder
     void SetZeroOffset();
+
+    // Simulation support
     void Simulate(units::degree_t angle);
 
+    /**
+     * @return adjusted degrees from absolute zero
+     */
     units::degree_t Get();
+
+    /**
+     * @return raw quadrature encoder ticks
+     */
     int GetRawQuad();
+
+    /**
+     * @return raw absolute encoder reading
+     */
     double GetRawAbs();
 
 private:
